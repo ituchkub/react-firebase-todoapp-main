f603381ca2f1482102720959d7849b140c34c6c3
ituchkub <ituchkub@hotmail.com>
2022-11-10 13:43:32 +0700

    New

---
+++ src/utils/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+  export default firebase;
